47ce1e8ccbf230f208fd8c646aa5a532bf6601bc
Pierre <pierre@yaska.eu>
2013-05-22 18:28:41 +0200

    routes

--- app/js/app.js
+++ app/js/app.js
@@ -8,6 +8,38 @@ angular.module('Dynomite', ['Dynomite.filters', 'Dynomite.services', 'Dynomite.d
       templateUrl: 'partials/phone-detail.html', 
       controller: PhoneDetailCtrl
     })
+    .when('/phones/:phoneId', {
+      templateUrl: 'partials/phone-detail.html', 
+      controller: PhoneDetailCtrl
+    })
+    .when('/area/:id', {
+      templateUrl: 'partials/areaId.html', 
+      controller: RouteIdCtrl
+    })
+    .when('/area/:id/pictures', {
+      templateUrl: 'partials/areaIdPictures.html', 
+      controller: RouteIdPicturesCtrl
+    })
+    .when('/area/:id/edit', {
+      templateUrl: 'partials/areaIdEdit.html', 
+      controller: RouteIdEditCtrl
+    })
+    .when('/area/add', {
+      templateUrl: 'partials/areaAdd.html', 
+      controller: RouteAddCtrl
+    })
+    .when('/route/:id', {
+      templateUrl: 'partials/routeId.html', 
+      controller: RouteIdCtrl
+    })
+    .when('/route/:id/edit', {
+      templateUrl: 'partials/routeIdEdit.html', 
+      controller: RouteIdEditCtrl
+    })
+    .when('/route/add', {
+      templateUrl: 'partials/routeAdd.html', 
+      controller: RouteAddCtrl
+    })
     .otherwise({
       redirectTo: '/phones'
     });
