14420671da9085906f4133f96fb8088d6dba1f52
Pierre <pierre@yaska.eu>
2013-05-22 18:20:50 +0200

    placeholders

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,14 @@
-angular.module('Dynomite', []).
+angular.module('Dynomite', ['Dynomite.filters', 'Dynomite.services', 'Dynomite.directives']).
   config(['$routeProvider', function($routeProvider) {
-    
+    .when('/phones', {
+      templateUrl: 'partials/phone-list.html',   
+      controller: PhoneListCtrl
+    })
+    .when('/phones/:phoneId', {
+      templateUrl: 'partials/phone-detail.html', 
+      controller: PhoneDetailCtrl
+    })
+    .otherwise({
+      redirectTo: '/phones'
+    });
   }]);
