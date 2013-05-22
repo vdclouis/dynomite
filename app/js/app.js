059ac354436cbefc62112cd2c41726e646d2d774
Louis Van de Calseyde <Louis@mbsp.local>
2013-05-22 19:10:59 +0200

    test keer pierre

--- app/js/app.js
+++ app/js/app.js
@@ -1,50 +1,50 @@
-angular.module('Dynomite', ['Dynomite.filters', 'Dynomite.services', 'Dynomite.directives']).
+angular.module('Dynomite', ['Dynomite.filters', 'Dynomite.services', 'Dynomite.directives', 'Dynomite.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    .when('/', {
-      templateUrl: 'partials/home.html',   
-      controller: HomeCtrl
-    })
-    .when('/area', {
-      templateUrl: 'partials/area.html', 
-      controller: AreaCtrl
-    })
-    .when('/area/map' {
+    $routeProvider.when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'HomeCtrl'
+    });
+    $routeProvider.when('/area', {
+      templateUrl: 'partials/area.html',
+      controller: 'AreaCtrl'
+    });
+    $routeProvider.when('/area/map', {
       templateUrl: 'partials/area-map.html',
-      controller: AreaMapCtrl
-    })
-    .when('/area/list' {
+      controller: 'AreaMapCtrl'
+    });
+    $routeProvider.when('/area/list', {
       templateUrl: 'partials/area-list.html',
-      controller: AreaListCtrl
-    })
-    .when('/area/:id', {
-      templateUrl: 'partials/areaId.html', 
-      controller: RouteIdCtrl
-    })
-    .when('/area/:id/pictures', {
-      templateUrl: 'partials/areaIdPictures.html', 
-      controller: RouteIdPicturesCtrl
-    })
-    .when('/area/:id/edit', {
-      templateUrl: 'partials/areaIdEdit.html', 
-      controller: RouteIdEditCtrl
-    })
-    .when('/area/add', {
-      templateUrl: 'partials/areaAdd.html', 
-      controller: RouteAddCtrl
-    })
-    .when('/route/:id', {
-      templateUrl: 'partials/routeId.html', 
-      controller: RouteIdCtrl
-    })
-    .when('/route/:id/edit', {
-      templateUrl: 'partials/routeIdEdit.html', 
-      controller: RouteIdEditCtrl
-    })
-    .when('/route/add', {
-      templateUrl: 'partials/routeAdd.html', 
-      controller: RouteAddCtrl
-    })
-    .otherwise({
+      controller: 'AreaListCtrl'
+    });
+    $routeProvider.when('/area/:id', {
+      templateUrl: 'partials/areaId.html',
+      controller: 'RouteIdCtrl'
+    });
+    $routeProvider.when('/area/:id/pictures', {
+      templateUrl: 'partials/areaIdPictures.html',
+      controller: 'RouteIdPicturesCtrl'
+    });
+    $routeProvider.when('/area/:id/edit', {
+      templateUrl: 'partials/areaIdEdit.html',
+      controller: 'RouteIdEditCtrl'
+    });
+    $routeProvider.when('/area/add', {
+      templateUrl: 'partials/areaAdd.html',
+      controller: 'RouteAddCtrl'
+    });
+    $routeProvider.when('/route/:id', {
+      templateUrl: 'partials/routeId.html',
+      controller: 'RouteIdCtrl'
+    });
+    $routeProvider.when('/route/:id/edit', {
+      templateUrl: 'partials/routeIdEdit.html',
+      controller: 'RouteIdEditCtrl'
+    });
+    $routeProvider.when('/route/add', {
+      templateUrl: 'partials/routeAdd.html',
+      controller: 'RouteAddCtrl'
+    });
+    $routeProvider.otherwise({
       redirectTo: '/'
     });
   }]);
---
+++ app/js/filters.js
@@ -0,0 +1,6 @@
+angular.module('Dynomite.filters', []).
+  filter('interpolate', ['version', function(version) {
+    return function(text) {
+      return String(text).replace(/\%VERSION\%/mg, version);
+    }
+  }]);
