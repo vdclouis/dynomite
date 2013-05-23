0c7a215427366092405a98f10ed1351933d0a7cf
Louis Van de Calseyde <Louis@mbsp.local>
2013-05-23 17:19:51 +0200

    routez

--- app/js/app.js
+++ app/js/app.js
@@ -1,70 +1,21 @@
 angular.module('Dynomite', ['Dynomite.filters', 'Dynomite.services', 'Dynomite.directives', 'leaflet-directive', 'Dynomite.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {
-      templateUrl: 'partials/home.html',
-      controller: 'HomeCtrl'
-    });
-    $routeProvider.when('/area', {
-      templateUrl: 'partials/area.html',
-      controller: 'AreaCtrl'
-    });
-    $routeProvider.when('/area/map', {
-      templateUrl: 'partials/area-map.html',
-      controller: 'AreaMapCtrl'
-    });
-    $routeProvider.when('/area/list', {
-      templateUrl: 'partials/area-list.html',
-      controller: 'AreaListCtrl'
-    });
-    $routeProvider.when('/area/:id', {
-      templateUrl: 'partials/areaId.html',
-      controller: 'RouteIdCtrl'
-    });
-    $routeProvider.when('/area/:id/pictures', {
-      templateUrl: 'partials/areaIdPictures.html',
-      controller: 'RouteIdPicturesCtrl'
-    });
-    $routeProvider.when('/area/:id/edit', {
-      templateUrl: 'partials/areaIdEdit.html',
-      controller: 'RouteIdEditCtrl'
-    });
-    $routeProvider.when('/area/add', {
-      templateUrl: 'partials/areaAdd.html',
-      controller: 'RouteAddCtrl'
-    });
-    $routeProvider.when('/route/:id', {
-      templateUrl: 'partials/routeId.html',
-      controller: 'RouteIdCtrl'
-    });
-    $routeProvider.when('/route/:id/edit', {
-      templateUrl: 'partials/routeIdEdit.html',
-      controller: 'RouteIdEditCtrl'
-    });
-    $routeProvider.when('/route/add', {
-      templateUrl: 'partials/routeAdd.html',
-      controller: 'RouteAddCtrl'
-    });
-    $routeProvider.when('/user', {
-      templateUrl: 'partials/user.html',
-      controller: 'UserCtrl'
-    });
-    $routeProvider.when('/user/:id', {
-      templateUrl: 'partials/userId.html',
-      controller: 'UserIdCtrl'
-    });
-    $routeProvider.when('/user/:id/edit', {
-      templateUrl: 'partials/userIdEdit.html',
-      controller: 'UserIdEditCtrl'
-    });
-    $routeProvider.when('/login', {
-      templateUrl: 'partials/login.html',
-      controller: 'LoginCtrl'
-    });
-    $routeProvider.when('/register', {
-      templateUrl: 'partials/register.html',
-      controller: 'RegisterCtrl'
-    });
-    $routeProvider.otherwise({
-      redirectTo: '/'
-    });
+    $routeProvider
+      .when('/',                  {templateUrl: 'partials/home.html', controller: 'HomeCtrl'})
+      .when('/area',              {templateUrl: 'partials/area.html', controller: 'AreaCtrl'})
+      .when('/area/map',          {templateUrl: 'partials/area-map.html', controller: 'AreaMapCtrl'})
+      .when('/area/list',         {templateUrl: 'partials/area-list.html', controller: 'AreaListCtrl'})
+      .when('/area/:id',          {templateUrl: 'partials/areaId.html', controller: 'RouteIdCtrl'})
+      .when('/area/:id/pictures', {templateUrl: 'partials/areaIdPictures.html', controller: 'RouteIdPicturesCtrl'})
+      .when('/area/:id/edit',     {templateUrl: 'partials/areaIdEdit.html', controller: 'RouteIdEditCtrl'})
+      .when('/area/add',          {templateUrl: 'partials/areaAdd.html', controller: 'RouteAddCtrl'})
+      .when('/route/:id',         {templateUrl: 'partials/routeId.html',controller: 'RouteIdCtrl'})
+      .when('/route/:id/edit',    {templateUrl: 'partials/routeIdEdit.html',controller: 'RouteIdEditCtrl'})
+      .when('/route/add',         {templateUrl: 'partials/routeAdd.html',controller: 'RouteAddCtrl'})
+      .when('/user',              {templateUrl: 'partials/user.html',controller: 'UserCtrl'})
+      .when('/user/:id',          {templateUrl: 'partials/userId.html',controller: 'UserIdCtrl'})
+      .when('/user/:id/edit',     {templateUrl: 'partials/userIdEdit.html',controller: 'UserIdEditCtrl'})
+      .when('/login',             {templateUrl: 'partials/login.html',controller: 'LoginCtrl'})
+      .when('/register',          {templateUrl: 'partials/register.html',controller: 'RegisterCtrl'})
+      .otherwise(                 {redirectTo: '/'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,6 +9,22 @@ angular.module('Dynomite.controllers', []).
       .success(function(data) {
         $scope.areas = data;
       });
+    angular.extend($scope, {
+      center: {
+        lat: 51.0500,
+        lng: 3.7167,
+        zoom: 4
+      },
+      markers: {
+        Madrid: {
+          lat: 40.095,
+          lng: -3.823,
+          message: "Drag me to your position",
+          focus: true,
+          draggable: true
+        }
+      }
+    });
   }])
   .controller('AreaMapCtrl', [function() {
   }])
