471b3f5bb38a4d0b0a9d3f32f18f59cdf45ccb50
Louis Van de Calseyde <vdc.louis@gmail.com>
2013-08-27 09:16:28 +0200

    compass

--- Gruntfile.js
+++ Gruntfile.js
@@ -140,7 +140,7 @@ module.exports = function (grunt) {
     compass: {
       options: {
         sassDir: '<%= yeoman.app %>/styles',
-        cssDir: '.tmp/styles',
+        cssDir: '<%= yeoman.app %>/styles/.tmp/',
         generatedImagesDir: '.tmp/images/generated',
         imagesDir: '<%= yeoman.app %>/images',
         javascriptsDir: '<%= yeoman.app %>/scripts',
