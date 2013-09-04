4d46e46f2237e87cf6ec61e83227ce16a9ab63dd
Louis Van de Calseyde <vdc.louis@gmail.com>
2013-09-04 20:55:38 +0200

    snap it

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -7,10 +7,20 @@ $(document).ready(function() {
   var snapper = new Snap({
     element: document.getElementById('snapper'),
     disable: 'left',
-    touchToDrag: false
+    touchToDrag: false,
+    tapToClose: true
   });
 
   document.getElementById('open-right').addEventListener('click', function() {
+    if( snapper.state().state === 'right' ) {
+      snapper.close();
+    } else {
+      snapper.open('right');
+    }
+  });
+
+  $('.menu-li').click(function() {
+    console.log('click');
     if( snapper.state().state === 'right' ) {
       snapper.close();
     } else {
